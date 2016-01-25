32c93760a4bd3e64c0fd07439072820e030a8f47
konstantinzal <konstantin@peekily.com>
2016-01-25 17:43:12 +0100

    Initial commit

---
+++ app/src/main/java/com/peekily/menus/MainActivity.java
@@ -0,0 +1,93 @@
+package com.peekily.menus;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this,v);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "You Clicked " + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    }
+                });
+                popup.show();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.main_view);
+
+        switch(item.getItemId()){
+            case R.id.menu_red:
+                item.setChecked(true);
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_yellow:
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.YELLOW);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
